acd676e145758c5964d5632f21e2aab501c93221
sullaspqr <sulla.spqr78@gmail.com>
2023-09-12 10:12:46 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Változók
+
+// var, let, const
+// var = global scope, let, const = block scope
+/*
+{
+    var valtozo1;
+    let valtozo2 = Number(2);
+    const valtozo3 = 3;
+    valtozo2 = 2.5;
+    console.log(valtozo1,valtozo2, valtozo3);
+    let valtozo4, valtozo5;
+}
+
+// pl. a = 1, b = a; log => a, b; b = 2; log => a, b
+// a = [1, 2, 3] , b = a; log => a, b; b[3] = 4; log => a, b
+
+// Konstans tömbök
+
+let tomb = ["a", "b", "c"];
+tomb[0] = "A"; // Ez lehetséges
+//tomb = ["a", "b", "c"]; Ez nem
+console.log(tomb);
+console.log(tomb[3]);
+*/
+let a = 2;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a**b);
